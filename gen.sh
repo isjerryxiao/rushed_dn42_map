0b712bfc3bab28c8fcdceb3fb11576195ecb721e
Jerry <isjerryxiao@outlook.com>
2021-03-06 17:04:09 +0800

    actions

--- gen.sh
+++ gen.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -ex
-python3 -m venv v
-v/bin/pip install -r req.txt
 [ -e registry ] && (cd registry; git pull) || git clone https://git.jerryxiao.cc/sync/dn42_registry.git registry --depth 1 --single-branch
 echo show r all table master4 table master6 |ssh -J $HOST shell@fd42:4242:2601:ac12::1 > grc.txt
+[ -z "$CI" ] && {
+python3 -m venv v
+v/bin/pip install -r req.txt
 v/bin/python map.py
+} || python3 map.py
