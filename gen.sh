470141a10ae2f47426c4fbdd27d08e80bfbbe9fb
Jerry <isjerryxiao@outlook.com>
2021-03-29 14:04:10 +0800

    use official registry

--- gen.sh
+++ gen.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -ex
-[ -e registry ] && (cd registry; git pull) || git clone https://git.jerryxiao.cc/sync/dn42_registry.git registry --depth 1 --single-branch
+[ -n "$GIT_DN42_TOKEN" ] && registry_path="https://${GIT_DN42_TOKEN}@git.dn42.dev/dn42/registry.git" || registry_path="https://git.jerryxiao.cc/sync/dn42_registry.git"
+[ -e registry ] && (cd registry; git pull) || git clone "$registry_path" registry --depth 1 --single-branch
 [ -e parsed.jsonl.bz2 ] && rm parsed.jsonl.bz2
 if [ -z "$CI" ]; then
     python3 -m venv v
