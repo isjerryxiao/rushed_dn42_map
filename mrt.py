a01d1215ffd448fa1bee141c50a7c4221ff1e1fd
Jerry <isjerryxiao@outlook.com>
2021-04-08 11:21:12 +0800

    fix crash when as_path > 255

--- mrt.py
+++ mrt.py
@@ -41,8 +41,11 @@ def process_entry(entry: mrtparse.Reader) -> dict:
             for attr in rib_entry['path_attributes']:
                 attr_type = attr['type'][0]
                 if attr_type == 2:
-                    assert len(attr['value']) == 1
-                    rib_attr['as_path'] = attr['value'][0]['value']
+                    parsed_as_path = list()
+                    for as_sequence in attr['value']:
+                        assert as_sequence['type'][0] == 2
+                        parsed_as_path.extend(as_sequence['value'])
+                    rib_attr['as_path'] = parsed_as_path
                 elif attr_type == 8:
                     rib_attr['community'] = attr['value']
                 elif attr_type == 16:
